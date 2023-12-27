72bf9866a7ea73051c92f24987f615611d7afd30
David Rieger <david@isan.engineer>
2023-12-27 21:14:59 +0100

    feat: add day 11 part 2

--- src/main/scala/advent/Day11.scala
+++ src/main/scala/advent/Day11.scala
@@ -16,8 +16,10 @@ object Day11 {
                    |#...#.....""".stripMargin.split("\n").toList
 
   val prod = Source.fromResource("day11_1.txt").getLines.toList
+
   case class Universe(raw: List[String]) {
-    lazy val expanded: Universe = Universe(raw.flatMap {
+
+    lazy val simpleExpanded: Universe = Universe(raw.flatMap {
         line =>
           if (line.contains('#')) List(line)
           else List(line, line)
@@ -27,14 +29,31 @@ object Day11 {
           else List(line, line)
       }.transpose.map(_.mkString))
 
-    lazy val galaxies: List[(Int, Int)] = raw.zipWithIndex.flatMap{
+    lazy val emptyRowIdxs = {
+      raw.zipWithIndex.collect{
+        case (line, y) if !line.contains('#') => y
+      }
+    }
+
+    lazy val emptyColumnIdxs = {
+      raw.transpose.zipWithIndex.collect {
+        case (line, x) if !line.contains('#') => x
+      }
+    }
+
+    def smartGalaxies(multiplier: Int): List[(Long, Long)] = simpleGalaxies.map{
+        case (x, y) => (x.toLong + emptyColumnIdxs.count(_ < x) * multiplier, y.toLong + emptyRowIdxs.count(_ < y) * multiplier)
+      }
+
+    lazy val simpleGalaxies: List[(Int, Int)] = raw.zipWithIndex.flatMap{
       case (line, y) =>
         line.zipWithIndex.collect{
           case (char, x) if (char == '#') => List((x, y))
         }.flatten
     }
 
-    lazy val distances = {
+
+    def distances(galaxies: List[(Long, Long)]) = {
       galaxies.flatMap{
         left => galaxies.map{
            right =>
@@ -49,9 +68,9 @@ object Day11 {
 
   val run = {
 
-    val expandedUniverse = Universe(prod).expanded
-    println(expandedUniverse)
-    expandedUniverse.distances.sum / 2
+    val universe = Universe(prod)
+    println(universe)
+    universe.distances(universe.smartGalaxies(999999)).sum / 2
 
   }
 
